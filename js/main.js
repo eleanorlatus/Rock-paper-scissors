ce5aea0b9ee4c151914563e3e7aa1da299f3e02e
Eleanor Latus <eleanor@latus.co.uk>
2022-06-26 16:41:12 +0100

    building base files

---
+++ js/main.js
@@ -0,0 +1,16 @@
+//psuedo-code
+// bot that randomly plays RPS
+// allow user to select choice using onclick
+// conditional that decides who has won and adds 1 to the tally
+// reset button
+
+
+let rock = document.getElementById("rock")
+let paper = document.getElementById("paper")
+let scissors = document.getElementById("scissors")
+
+rock.onclick = showRock
+
+function showRock(){
+
+}
